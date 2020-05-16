efd36ee8e837c5dab94282bf708a460ba3a48c52
sunghyuk-seo <64149171+sunghyuk-seo@users.noreply.github.com>
2020-05-16 15:41:45 +0900

    project

--- app/src/main/java/com/example/todak/Intro/ThirdFragment.java
+++ app/src/main/java/com/example/todak/Intro/ThirdFragment.java
@@ -9,7 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
-import com.example.todak.ViewHome.Main2Activity;
+
 import com.example.todak.R;
 
 public class ThirdFragment  extends Fragment  {
