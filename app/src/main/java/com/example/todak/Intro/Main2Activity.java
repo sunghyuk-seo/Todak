5d7ab8a3dc01a793d3943541feb9dcc20f608070
Sonak0930 <59792911+Sonak0930@users.noreply.github.com>
2020-05-16 17:44:03 +0900

    #차재민 Doubt 버튼 구현

--- app/src/main/java/com/example/todak/Intro/Main2Activity.java
+++ app/src/main/java/com/example/todak/Intro/Main2Activity.java
@@ -2,7 +2,11 @@ package com.example.todak.Intro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.example.todak.R;
 
@@ -12,5 +16,14 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        ImageButton button2 = (ImageButton)findViewById(R.id.doubt);
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this,Doubt_search.class);
+                startActivity(intent);
+            }
+        });
     }
 }
