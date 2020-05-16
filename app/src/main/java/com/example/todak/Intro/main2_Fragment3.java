aa659fda02782dbc3f6d78e1456a3aa6c3e21600
Sonak0930 <59792911+Sonak0930@users.noreply.github.com>
2020-05-16 16:49:05 +0900

    성혁형이 만든거 ?

---
+++ app/src/main/java/com/example/todak/Intro/main2_Fragment3.java
@@ -0,0 +1,53 @@
+package com.example.todak.Intro;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.todak.R;
+
+public class main2_Fragment3 extends Fragment  {
+    // Store instance variables
+    private String title;
+    private int page;
+
+    // newInstance constructor for creating fragment with arguments
+    public static main2_Fragment3 newInstance(int page, String title) {
+        main2_Fragment3 fragment = new main2_Fragment3();
+        Bundle args = new Bundle();
+        args.putInt("someInt", page);
+        args.putString("someTitle", title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    // Store instance variables based on arguments passed
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        page = getArguments().getInt("someInt", 0);
+        title = getArguments().getString("someTitle");
+
+
+
+
+    }
+
+    // Inflate the view for the fragment based on layout XML
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_main3, container, false);
+
+        return view;
+
+
+    }
+
+
+
+
+}
