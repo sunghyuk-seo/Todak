b6ebb47f412c8b997549dd0b66286b1ccc62a0aa
ojw4184 <64193469+ojw4184@users.noreply.github.com>
2020-05-18 00:11:10 +0900

    아이콘 크기 변경 + 예상질병검색 추가

--- app/src/main/java/com/example/todak/Intro/Main2Activity.java
+++ app/src/main/java/com/example/todak/Intro/Main2Activity.java
@@ -1,12 +1,11 @@
 package com.example.todak.Intro;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.todak.R;
 
@@ -31,7 +30,7 @@ public class Main2Activity extends AppCompatActivity {
         button2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Main2Activity.this,Doubt_search.class);
+                Intent intent = new Intent(Main2Activity.this, Disease_search.class);
                 startActivity(intent);
             }
         });
