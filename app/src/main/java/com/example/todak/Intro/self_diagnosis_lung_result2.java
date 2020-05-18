18aeb49349b54947f379e9a4ec639c78b5c96cdd
sunghyuk-seo <64149171+sunghyuk-seo@users.noreply.github.com>
2020-05-18 21:43:53 +0900

    자가진단 일단 구현

--- app/src/main/java/com/example/todak/Intro/self_diagnosis_lung_result2.java
+++ app/src/main/java/com/example/todak/Intro/self_diagnosis_lung_result2.java
@@ -2,7 +2,10 @@ package com.example.todak.Intro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.todak.R;
 
@@ -12,5 +15,23 @@ public class self_diagnosis_lung_result2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_self_diagnosis_lung_result2);
+        Button home = (Button)findViewById(R.id.home_button);
+        Button map = (Button)findViewById(R.id.path_button);
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(self_diagnosis_lung_result2.this, Main2Activity.class);
+                startActivity(intent);
+            }
+        });
+
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(self_diagnosis_lung_result2.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
