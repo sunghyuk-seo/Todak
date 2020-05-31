72229b2c62fd5d2e9a0176e6396e8452fd9c67cf
sunghyuk-seo <64149171+sunghyuk-seo@users.noreply.github.com>
2020-05-31 14:44:10 +0900

    5/31 수정

---
+++ app/src/main/java/com/example/todak/Intro/Main2Activity.java
@@ -0,0 +1,47 @@
+package com.example.todak.Intro;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.todak.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        ImageButton button1 = (ImageButton)findViewById(R.id.self);
+        ImageButton button2 = (ImageButton)findViewById(R.id.doubt);
+        ImageButton button3 = (ImageButton)findViewById(R.id.path);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, self_diagnosis.class);
+                startActivity(intent);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, Disease_search.class);
+                startActivity(intent);
+            }
+        });
+
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Main2Activity.this, MapsActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/todak/Intro/self_diagnosis_lung4.java
@@ -0,0 +1,38 @@
+package com.example.todak.Intro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.todak.R;
+
+public class self_diagnosis_lung4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_self_diagnosis_lung4);
+
+        Button yes = (Button)findViewById(R.id.yes_button);
+        Button no = (Button)findViewById(R.id.no_button);
+
+        yes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(self_diagnosis_lung4.this, self_diagnosis_lung5.class);
+                startActivity(intent);
+            }
+        });
+
+        no.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(self_diagnosis_lung4.this, self_diagnosis_lung5.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
