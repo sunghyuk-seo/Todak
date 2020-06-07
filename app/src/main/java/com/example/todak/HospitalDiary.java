926ce667a2c0359e9ee7c6bce063002fd75d04cb
sunghyuk-seo <64149171+sunghyuk-seo@users.noreply.github.com>
2020-06-07 16:07:52 +0900

    6.7

--- app/src/main/java/com/example/todak/HospitalDiary.java
+++ app/src/main/java/com/example/todak/HospitalDiary.java
@@ -17,11 +17,11 @@ import java.util.Calendar;
 
 public class HospitalDiary extends AppCompatActivity {
 
-    DatePicker datePicker;  //  datePicker - 날짜를 선택하는 달력
-    TextView viewDatePick;  //  viewDatePick - 선택한 날짜를 보여주는 textView
-    EditText edtDiary;   //  edtDiary - 선택한 날짜의 일기를 쓰거나 기존에 저장된 일기가 있다면 보여주고 수정하는 영역
-    Button btnSave;   //  btnSave - 선택한 날짜의 일기 저장 및 수정(덮어쓰기) 버튼
-    String fileName;   //  fileName - 돌고 도는 선택된 날짜의 파일 이름
+    DatePicker datePicker;
+    TextView viewDatePick;
+    EditText edtDiary;
+    Button btnSave;
+    String fileName;
     Button btnDelete;
 
     protected void onCreate(Bundle savedInstanceState) {
@@ -62,15 +62,11 @@ public class HospitalDiary extends AppCompatActivity {
                 FileOutputStream fos = null;
 
                 try{
-                    fos = openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS); //MODE_WORLD_WRITEABLE
+                    fos = openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS);
                     String content = edtDiary.getText().toString();
-
-                    // String.getBytes() = 스트링을 배열형으로 변환?
                     fos.write(content.getBytes());
-                    //fos.flush();
                     fos.close();
 
-                    // getApplicationContext() = 현재 클래스.this ?
                     Toast.makeText(getApplicationContext(), "일기 저장됨", Toast.LENGTH_SHORT).show();
 
                 } catch (Exception e) {
@@ -113,14 +109,10 @@ public class HospitalDiary extends AppCompatActivity {
     }
 
     private void checkedDay(int year, int monthOfYear, int dayOfMonth) {
-        // 받은 날짜로 날짜 보여주는
         viewDatePick.setText(year + " - " + monthOfYear + " - " + dayOfMonth);
 
-        // 파일 이름을 만들어준다. 파일 이름은 "20170318.txt" 이런식으로 나옴
         fileName = year + "" + monthOfYear + "" + dayOfMonth + ".txt";
 
-        // 읽어봐서 읽어지면 일기 가져오고
-        // 없으면 catch 그냥 살아? 아주 위험한 생각같다..
         FileInputStream fis = null;
         try {
             fis = openFileInput(fileName);
@@ -128,12 +120,10 @@ public class HospitalDiary extends AppCompatActivity {
             fis.read(fileData);
             fis.close();
             String str = new String(fileData, "UTF-8");
-            // 읽어서 토스트 메시지로 보여줌
             Toast.makeText(getApplicationContext(), "병원 갔던 날", Toast.LENGTH_SHORT).show();
             edtDiary.setText(str);
             btnSave.setText("일기 저장");
-        } catch (Exception e) { // UnsupportedEncodingException , FileNotFoundException , IOException
-            // 없어서 오류가 나면 일기가 없는 것 -> 일기를 쓰게 한다.
+        } catch (Exception e) {
             Toast.makeText(getApplicationContext(), "병원 안 갔던 날", Toast.LENGTH_SHORT).show();
             edtDiary.setText("");
             btnSave.setText("일기 저장");
