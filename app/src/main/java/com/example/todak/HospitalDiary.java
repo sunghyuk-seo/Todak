0fab27d57d7b1ed625d9423c79db793c989af464
ojw4184 <64193469+ojw4184@users.noreply.github.com>
2020-06-07 14:02:55 +0900

    Diary 구현

--- app/src/main/java/com/example/todak/HospitalDiary.java
+++ app/src/main/java/com/example/todak/HospitalDiary.java
@@ -1,4 +1,116 @@
 package com.example.todak;
 
-public class HospitalDiary {
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.Calendar;
+
+public class HospitalDiary extends AppCompatActivity {
+
+    DatePicker datePicker;  //  datePicker - 날짜를 선택하는 달력
+    TextView viewDatePick;  //  viewDatePick - 선택한 날짜를 보여주는 textView
+    EditText edtDiary;   //  edtDiary - 선택한 날짜의 일기를 쓰거나 기존에 저장된 일기가 있다면 보여주고 수정하는 영역
+    Button btnSave;   //  btnSave - 선택한 날짜의 일기 저장 및 수정(덮어쓰기) 버튼
+    String fileName;   //  fileName - 돌고 도는 선택된 날짜의 파일 이름
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_diary);
+
+        datePicker = (DatePicker) findViewById(R.id.datePicker);
+        viewDatePick = (TextView) findViewById(R.id.viewDatePick);
+        edtDiary = (EditText) findViewById(R.id.edtDiary);
+        btnSave = (Button) findViewById(R.id.btnSave);
+
+        Calendar c = Calendar.getInstance();
+        int cYear = c.get(Calendar.YEAR);
+        int cMonth = c.get(Calendar.MONTH);
+        int cDay = c.get(Calendar.DAY_OF_MONTH);
+
+        checkedDay(cYear, cMonth, cDay);
+
+        datePicker.init(datePicker.getYear(), datePicker.getMonth(),
+                datePicker.getDayOfMonth(), new DatePicker.OnDateChangedListener() {
+                    @Override
+                    public void onDateChanged(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        checkedDay(year, monthOfYear, dayOfMonth);
+                    }
+                });
+
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveDiary(fileName);
+            }
+
+            @SuppressLint("WrongConstant")
+            private void saveDiary(String readDay) {
+
+                FileOutputStream fos = null;
+
+                try{
+                    fos = openFileOutput(readDay, MODE_NO_LOCALIZED_COLLATORS); //MODE_WORLD_WRITEABLE
+                    String content = edtDiary.getText().toString();
+
+                    // String.getBytes() = 스트링을 배열형으로 변환?
+                    fos.write(content.getBytes());
+                    //fos.flush();
+                    fos.close();
+
+                    // getApplicationContext() = 현재 클래스.this ?
+                    Toast.makeText(getApplicationContext(), "일기 저장됨", Toast.LENGTH_SHORT).show();
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplicationContext(), "오류오류", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+
+
+    }
+
+    private void checkedDay(int year, int monthOfYear, int dayOfMonth) {
+        // 받은 날짜로 날짜 보여주는
+        viewDatePick.setText(year + " - " + monthOfYear + " - " + dayOfMonth);
+
+        // 파일 이름을 만들어준다. 파일 이름은 "20170318.txt" 이런식으로 나옴
+        fileName = year + "" + monthOfYear + "" + dayOfMonth + ".txt";
+
+        // 읽어봐서 읽어지면 일기 가져오고
+        // 없으면 catch 그냥 살아? 아주 위험한 생각같다..
+        FileInputStream fis = null;
+        try {
+            fis = openFileInput(fileName);
+
+            byte[] fileData = new byte[fis.available()];
+            fis.read(fileData);
+            fis.close();
+
+            String str = new String(fileData, "EUC-KR");
+            // 읽어서 토스트 메시지로 보여줌
+            Toast.makeText(getApplicationContext(), "일기 써둔 날", Toast.LENGTH_SHORT).show();
+            edtDiary.setText(str);
+            btnSave.setText("수정하기");
+        } catch (Exception e) { // UnsupportedEncodingException , FileNotFoundException , IOException
+            // 없어서 오류가 나면 일기가 없는 것 -> 일기를 쓰게 한다.
+            Toast.makeText(getApplicationContext(), "일기 없는 날", Toast.LENGTH_SHORT).show();
+            edtDiary.setText("");
+            btnSave.setText("새 일기 저장");
+            e.printStackTrace();
+        }
+    }
+
+
 }
