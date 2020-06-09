572266acb79dc0be95a84f4b7a91f3258a92c614
sunghyuk-seo <64149171+sunghyuk-seo@users.noreply.github.com>
2020-06-10 05:07:35 +0900

    6.10 자가진단 구현

--- app/src/main/java/com/example/todak/self_diagnosis_lung.java
+++ app/src/main/java/com/example/todak/self_diagnosis_lung.java
@@ -2,97 +2,239 @@ package com.example.todak;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class self_diagnosis_lung extends AppCompatActivity {
+
+    public static Boolean var;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_self_diagnosis_lung);
 
-        final CheckBox cb1 = (CheckBox)findViewById(R.id.checkBox1);
-        final CheckBox cb2 = (CheckBox)findViewById(R.id.checkBox2);
-        final CheckBox cb3 = (CheckBox)findViewById(R.id.checkBox3);
-        final CheckBox cb4 = (CheckBox)findViewById(R.id.checkBox4);
-        final CheckBox cb5 = (CheckBox)findViewById(R.id.checkBox5);
-        final CheckBox cb6 = (CheckBox)findViewById(R.id.checkBox6);
-        final CheckBox cb7 = (CheckBox)findViewById(R.id.checkBox7);
-        final CheckBox cb8 = (CheckBox)findViewById(R.id.checkBox8);
-        final CheckBox cb9 = (CheckBox)findViewById(R.id.checkBox9);
-        final CheckBox cb10 = (CheckBox)findViewById(R.id.checkBox10);
-        final CheckBox cb11 = (CheckBox)findViewById(R.id.checkBox11);
-        final CheckBox cb12 = (CheckBox)findViewById(R.id.checkBox12);
-        final CheckBox cb13 = (CheckBox)findViewById(R.id.checkBox13);
-        final CheckBox cb14 = (CheckBox)findViewById(R.id.checkBox14);
-        final CheckBox cb15 = (CheckBox)findViewById(R.id.checkBox15);
-        final CheckBox cb16 = (CheckBox)findViewById(R.id.checkBox16);
-        final CheckBox cb17 = (CheckBox)findViewById(R.id.checkBox17);
-        final CheckBox cb18 = (CheckBox)findViewById(R.id.checkBox18);
-        final CheckBox cb19 = (CheckBox)findViewById(R.id.checkBox19);
-        final CheckBox cb20 = (CheckBox)findViewById(R.id.checkBox20);
-        final CheckBox cb21 = (CheckBox)findViewById(R.id.checkBox21);
-        final CheckBox cb22 = (CheckBox)findViewById(R.id.checkBox22);
-        final CheckBox cb23 = (CheckBox)findViewById(R.id.checkBox23);
-        final CheckBox cb24 = (CheckBox)findViewById(R.id.checkBox24);
+        var = false;
 
+        final CheckBox cb1 = (CheckBox) findViewById(R.id.checkBox1);
+        final CheckBox cb2 = (CheckBox) findViewById(R.id.checkBox2);
+        final CheckBox cb3 = (CheckBox) findViewById(R.id.checkBox3);
+        final CheckBox cb4 = (CheckBox) findViewById(R.id.checkBox4);
+        final CheckBox cb5 = (CheckBox) findViewById(R.id.checkBox5);
+        final CheckBox cb6 = (CheckBox) findViewById(R.id.checkBox6);
+        final CheckBox cb7 = (CheckBox) findViewById(R.id.checkBox7);
+        final CheckBox cb8 = (CheckBox) findViewById(R.id.checkBox8);
+        final CheckBox cb9 = (CheckBox) findViewById(R.id.checkBox9);
+        final CheckBox cb10 = (CheckBox) findViewById(R.id.checkBox10);
+        final CheckBox cb11 = (CheckBox) findViewById(R.id.checkBox11);
+        final CheckBox cb12 = (CheckBox) findViewById(R.id.checkBox12);
+        final CheckBox cb13 = (CheckBox) findViewById(R.id.checkBox13);
+        final CheckBox cb14 = (CheckBox) findViewById(R.id.checkBox14);
+        final CheckBox cb15 = (CheckBox) findViewById(R.id.checkBox15);
+        final CheckBox cb16 = (CheckBox) findViewById(R.id.checkBox16);
+        final CheckBox cb17 = (CheckBox) findViewById(R.id.checkBox17);
+        final CheckBox cb18 = (CheckBox) findViewById(R.id.checkBox18);
+        final CheckBox cb19 = (CheckBox) findViewById(R.id.checkBox19);
+        final CheckBox cb20 = (CheckBox) findViewById(R.id.checkBox20);
+        final CheckBox cb21 = (CheckBox) findViewById(R.id.checkBox21);
+        final CheckBox cb22 = (CheckBox) findViewById(R.id.checkBox22);
+        final CheckBox cb23 = (CheckBox) findViewById(R.id.checkBox23);
+        final CheckBox cb24 = (CheckBox) findViewById(R.id.checkBox24);
 
-        Button b = (Button)findViewById(R.id.button1);
+        cb1.setChecked(false);
+        cb2.setChecked(false);
+        cb3.setChecked(false);
+        cb4.setChecked(false);
+        cb5.setChecked(false);
+        cb6.setChecked(false);
+        cb7.setChecked(false);
+        cb8.setChecked(false);
+        cb9.setChecked(false);
+        cb10.setChecked(false);
+        cb11.setChecked(false);
+        cb12.setChecked(false);
+        cb13.setChecked(false);
+        cb14.setChecked(false);
+        cb15.setChecked(false);
+        cb16.setChecked(false);
+        cb17.setChecked(false);
+        cb18.setChecked(false);
+        cb19.setChecked(false);
+        cb20.setChecked(false);
+        cb21.setChecked(false);
+        cb22.setChecked(false);
+        cb23.setChecked(false);
+        cb24.setChecked(false);
 
-
-
-
-        b.setOnClickListener(new View.OnClickListener() {
-
+        Button submit = (Button) findViewById(R.id.submit);
+        submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int corona = 0; // 코로나
-                int cold = 0; // 감기
-                int pneumonia = 0; //폐렴
-                int tuberculosis = 0; // 폐결핵
-                int asthma = 0; //천식
-                int acute_bronchitis = 0; //급성기관지염
-                int pneumothorax = 0; //기흉
-                int allergic_coryza = 0; //알레르기성 비염
-                int hyperventilation_syndrome = 0; // 과호흡증후군
+                float cold = 0; // 감기
+                float corona = 0; // 코로나
+                float pneumonia = 0; //폐렴
+                float tuberculosis = 0; // 폐결핵
+                float asthma = 0; //천식
+                float acute_bronchitis = 0; //급성기관지염
+                float pneumothorax = 0; //기흉
+                float allergic_coryza = 0; //알레르기성 비염
+                float hyperventilation_syndrome = 0; // 과호흡증후군
 
-                if(cb1.isChecked() == true)  {
+
+                if (cb1.isChecked()) {
                     corona += 1;
                     cold += 1;
                     pneumonia += 1;
                     tuberculosis += 1;
                 }
-                if(cb2.isChecked() == true) ;
-                if(cb3.isChecked() == true) ;
-                if(cb4.isChecked() == true) ;
-                if(cb5.isChecked() == true) ;
-                if(cb6.isChecked() == true) ;
-                if(cb7.isChecked() == true) ;
-                if(cb8.isChecked() == true) ;
-                if(cb9.isChecked() == true)
-                if(cb10.isChecked() == true)
-                if(cb11.isChecked() == true)
-                if(cb12.isChecked() == true)
-                if(cb13.isChecked() == true)
-                if(cb14.isChecked() == true)
-                if(cb15.isChecked() == true)
-                if(cb16.isChecked() == true)
-                if(cb17.isChecked() == true)
-                if(cb18.isChecked() == true)
-                if(cb19.isChecked() == true)
-                if(cb20.isChecked() == true)
-                if(cb21.isChecked() == true)
-                if(cb22.isChecked() == true)
-                if(cb23.isChecked() == true)
-                if(cb24.isChecked() == true);
+                if (cb2.isChecked()) {
+                    cold += 1;
+                    pneumonia += 1;
+                }
+                if (cb3.isChecked()) {
+                    corona += 1;
+                    cold += 1;
+                    tuberculosis += 1;
+                    asthma += 1;
+                    acute_bronchitis += 1;
+                    pneumothorax += 1;
+                    allergic_coryza += 1;
+                }
+                if (cb4.isChecked()) {
+                    corona += 1;
+                    cold += 1;
+                    tuberculosis += 1;
+                    pneumothorax += 1;
+                }
+                if (cb5.isChecked()) {
+                    corona += 1;
+                    cold += 1;
+                }
+                if (cb6.isChecked()) {
+                    corona += 1;
+                    cold += 1;
+                    tuberculosis += 1;
+                }
+                if (cb7.isChecked()) {
+                    corona += 1;
+                    asthma += 1;
+                    tuberculosis += 1;
+                    pneumonia += 1;
+                    pneumothorax += 1;
+                    allergic_coryza += 1;
+                }
+                if (cb8.isChecked()) {
+                    pneumothorax += 1;
+                }
+                if (cb9.isChecked()) {
+                    cold += 1;
+                    tuberculosis += 1;
+                }
+                if (cb10.isChecked()) {
+                    corona += 1;
+                    pneumonia += 1;
+                    asthma += 1;
+                    acute_bronchitis += 1;
+                    pneumothorax += 1;
+                    hyperventilation_syndrome += 1;
+                }
+                if (cb11.isChecked()) {
+                    cold += 1;
+                    pneumonia += 1;
+                    acute_bronchitis += 1;
+                    asthma += 1;
+                }
+                if (cb12.isChecked()) {
+                    corona += 1;
+                    cold += 1;
+                }
+                if (cb13.isChecked()) {
+                    corona += 1;
+                    cold += 1;
+                }
+                if (cb14.isChecked()) {
+                    cold += 1;
+                    allergic_coryza += 1;
+                }
+                if (cb15.isChecked()) {
+                    cold += 1;
+                    allergic_coryza += 1;
+                }
+                if (cb16.isChecked()) {
+                    cold += 1;
+                    allergic_coryza += 1;
+                }
+                if (cb17.isChecked()) {
+                    tuberculosis += 1;
+                }
+                if (cb18.isChecked()) {
+                    cold += 1;
+                    pneumonia += 1;
+                }
+                if (cb19.isChecked()) {
+                    acute_bronchitis += 1;
+                    asthma += 1;
+                }
+                if (cb20.isChecked()) {
+                    hyperventilation_syndrome += 1;
+                }
+                if (cb21.isChecked()) {
+                    hyperventilation_syndrome += 1;
+                }
+                if (cb22.isChecked()) {
+                    hyperventilation_syndrome += 1;
+                }
+                if (cb23.isChecked()) {
+                    hyperventilation_syndrome += 1;
+                }
+                if (cb24.isChecked()) {
+                    hyperventilation_syndrome += 1;
+                }
 
-            } // end onClick
+                String[] array = {String.valueOf((corona / 9) * 100), String.valueOf((cold / 13) * 100), String.valueOf((pneumonia / 5) * 100),
+                        String.valueOf((tuberculosis / 8) * 100), String.valueOf((asthma / 5) * 100), String.valueOf((acute_bronchitis / 4) * 100),
+                        String.valueOf((pneumothorax / 5) * 100), String.valueOf((allergic_coryza / 6) * 100), String.valueOf((hyperventilation_syndrome / 6) * 100)};
+
+                send(array);
+
+                cold = 0; // 감기
+                corona = 0; // 코로나
+                pneumonia = 0; //폐렴
+                tuberculosis = 0; // 폐결핵
+                asthma = 0; //천식
+                acute_bronchitis = 0; //급성기관지염
+                pneumothorax = 0; //기흉
+                allergic_coryza = 0; //알레르기성 비염
+                hyperventilation_syndrome = 0; // 과호흡증후군
+
+            }
+
+            void send(String[] array) {
+                Intent intent = new Intent(getApplicationContext(), self_diagnosis_lung_result.class);
+
+                intent.putExtra("감기", array[0]);
+                intent.putExtra("코로나", array[1]);
+                intent.putExtra("폐렴", array[2]);
+                intent.putExtra("폐결핵", array[3]);
+                intent.putExtra("천식", array[4]);
+                intent.putExtra("급성기관지염", array[5]);
+                intent.putExtra("기흉", array[6]);
+                intent.putExtra("알레르기성 비염", array[7]);
+                intent.putExtra("과호흡증후군", array[8]);
+
+
+                startActivity(intent);
+            }
+
+
         });
-
 
     }
 }
---
+++ app/src/main/java/com/example/todak/self_diagnosis_lung_result.java
@@ -0,0 +1,66 @@
+package com.example.todak;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class self_diagnosis_lung_result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_self_diagnosis_lung_result);
+
+        TextView cold = (TextView)findViewById(R.id.cold); /*TextView선언*/
+        TextView corona = (TextView)findViewById(R.id.corona);
+        TextView pneumonia = (TextView)findViewById(R.id.pneumonia);
+        TextView tuberculosis = (TextView)findViewById(R.id.tuberculosis);
+        TextView asthma = (TextView)findViewById(R.id.asthma);
+        TextView acute_bronchitis = (TextView)findViewById(R.id.acute_bronchitis);
+        TextView pneumothorax = (TextView)findViewById(R.id.pneumothorax);
+        TextView allergic_coryza  = (TextView)findViewById(R.id.allergic_coryza );
+        TextView hyperventilation_syndrome = (TextView)findViewById(R.id.hyperventilation_syndrome);
+
+        Intent intent = getIntent();
+
+        String Cold = intent.getExtras().getString("감기"); /*String형*/
+        cold.setText(cold + "%");
+
+        String Corona = intent.getExtras().getString("코로나"); /*String형*/
+        corona.setText(Corona + "%");
+
+        String Pneumonia = intent.getExtras().getString("폐렴"); /*String형*/
+        pneumonia.setText(Pneumonia + "%");
+
+        String Tuberculosis = intent.getExtras().getString("폐결핵"); /*String형*/
+        tuberculosis.setText(Tuberculosis + "%");
+
+        String Asthma = intent.getExtras().getString("천식"); /*String형*/
+        asthma.setText(Asthma + "%");
+
+        String Acute_Bronchitis = intent.getExtras().getString("급성기관지염"); /*String형*/
+        acute_bronchitis.setText(Acute_Bronchitis + "%");
+
+        String Pneumothorax = intent.getExtras().getString("기흉"); /*String형*/
+        pneumothorax.setText(Pneumothorax + "%");
+
+        String Allergic_Coryza = intent.getExtras().getString("알레르기성 비염"); /*String형*/
+        allergic_coryza.setText(Allergic_Coryza + "%");
+
+        String Hyperventilation = intent.getExtras().getString("과호흡증후군"); /*String형*/
+        hyperventilation_syndrome.setText(Hyperventilation + "%");
+
+        Button back = (Button) findViewById(R.id.back);
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(self_diagnosis_lung_result.this, self_diagnosis_lung.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
